99927dc2a57308713e274716ff4b6b3b658eebcb
200Tigersbloxed <45884377+200Tigersbloxed@users.noreply.github.com>
2023-03-27 13:08:30 -0400

    PlayerMessage and NetworkedEvent
    coolio

---
+++ Hypernex.Networking/Messages/NetworkedEvent.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Nexport;
+
+namespace Hypernex.Networking.Messages;
+
+/// <summary>
+/// Used to send data from a Client to a Server, or from a Server to a(n) client(s)
+/// </summary>
+[Msg]
+public class NetworkedEvent
+{
+    [MsgKey(1)] public string MessageId => typeof(NetworkedEvent).FullName;
+    // Optional if from Server to Client
+    [MsgKey(2)] public JoinAuth Auth;
+    /// <summary>
+    /// The name of the event to trigger
+    /// </summary>
+    [MsgKey(3)] public string EventName;
+    /// <summary>
+    /// The data to send between the server. This data MUST be serializable!
+    /// </summary>
+    [MsgKey(4)] public List<object> Data;
+
+    /// <summary>
+    /// Sends data from the specified Server to the specified client
+    /// </summary>
+    /// <param name="server">The server to send data from</param>
+    /// <param name="toClient">The ClientIdentifier to send data to</param>
+    /// <param name="eventName">The name of the Event to Invoke</param>
+    /// <param name="data">The data passed to the event</param>
+    /// <param name="messageChannel">What channel the message should be sent over</param>
+    public static void FireClient(Server server, ClientIdentifier toClient, string eventName, object[] data,
+        MessageChannel messageChannel = MessageChannel.Reliable)
+    {
+        NetworkedEvent networkedEvent = new NetworkedEvent
+        {
+            EventName = eventName,
+            Data = data.ToList()
+        };
+        server.SendMessage(toClient, Msg.Serialize(networkedEvent), messageChannel);
+    }
+
+    /// <summary>
+    /// Sends data from the specified Server to all of its connected clients
+    /// </summary>
+    /// <param name="server">The server to send data from</param>
+    /// <param name="eventName">The name of the Event to Invoke</param>
+    /// <param name="data">The data passed to the Event</param>
+    /// <param name="messageChannel">What channel the message should be sent over</param>
+    public static void FireAllClients(Server server, string eventName, object[] data,
+        MessageChannel messageChannel = MessageChannel.Reliable)
+    {
+        NetworkedEvent networkedEvent = new NetworkedEvent
+        {
+            EventName = eventName,
+            Data = data.ToList()
+        };
+        server.BroadcastMessage(Msg.Serialize(networkedEvent), messageChannel);
+    }
+
+    /// <summary>
+    /// Sends data from the specified Client to the connected Server
+    /// </summary>
+    /// <param name="client">The client to send data from</param>
+    /// <param name="eventName">The name of the Event to Invoke</param>
+    /// <param name="data">The data passed to the Event</param>
+    /// <param name="messageChannel">What channel the message should be sent over</param>
+    public static void FireServer(Client client, string eventName, object[] data,
+        MessageChannel messageChannel = MessageChannel.Reliable)
+    {
+        NetworkedEvent networkedEvent = new NetworkedEvent
+        {
+            EventName = eventName,
+            Data = data.ToList()
+        };
+        client.SendMessage(Msg.Serialize(networkedEvent), messageChannel);
+    }
+}
---
+++ Hypernex.Networking/Messages/PlayerMessage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Nexport;
+
+namespace Hypernex.Networking.Messages;
+
+/// <summary>
+/// A chat message that a Player wants to broadcast
+/// </summary>
+[Msg]
+public class PlayerMessage
+{
+    [MsgKey(1)] public string MessageId => typeof(PlayerMessage).FullName;
+    [MsgKey(2)] public JoinAuth Auth;
+    /// <summary>
+    /// Used to define optional tags of a message, such as who can and cannot see a message
+    /// </summary>
+    [MsgKey(3)] public List<string> MessageTags;
+    /// <summary>
+    /// The message to send
+    /// </summary>
+    [MsgKey(4)] public string Message;
+}
