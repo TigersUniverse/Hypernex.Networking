31976f6c066f2f41f67c5626de549fd4f538027d
200Tigersbloxed <45884377+200Tigersbloxed@users.noreply.github.com>
2024-10-08 23:26:59 -0400

    update time

--- Hypernex.Networking/SandboxedClasses/Time.cs
+++ Hypernex.Networking/SandboxedClasses/Time.cs
@@ -14,12 +14,12 @@ public class Time
         d = new DateTime(year, month, day, hour, minute, second, millisecond);
     public void Load(UtcTime u) => d = TimeZoneInfo.ConvertTimeFromUtc(u.d, TimeZoneInfo.Utc);
 
-    public bool Is24HourClock() => DateTimeFormatInfo.CurrentInfo.ShortTimePattern.Contains("H");
+    public bool Is24HourClock() => TimeSettings.Is24HourClock.Invoke();
 
     public string GetDayOfWeek() => d.DayOfWeek.ToString();
     public string GetMonthName() => d.ToString("MM");
     public string GetAMPM() => d.ToString("tt", CultureInfo.InvariantCulture).ToUpper();
-        
+    
     public int GetMilliseconds() => d.Millisecond;
     public int GetSeconds() => d.Second;
     public int GetMinutes() => d.Minute;
---
+++ Hypernex.Networking/SandboxedClasses/TimeSettings.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Globalization;
+
+namespace Hypernex.Sandboxing.SandboxedTypes;
+
+public static class TimeSettings
+{
+    public static Func<bool> Is24HourClock = () => DateTimeFormatInfo.CurrentInfo.ShortTimePattern.Contains("H");
+}
